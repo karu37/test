9afe573107695796b266cb0e8b95cd4bf3e959f3
heartman <heartman@gmail.com>
2016-07-19 17:10:09 +0900

    x

--- admin/home/admin-pages/merchant-campaign-modify.php
+++ admin/home/admin-pages/merchant-campaign-modify.php
@@ -6,7 +6,6 @@
 	$db_mcode = mysql_real_escape_string($mcode);
 	
 	$sql = "SELECT app.*, m.name AS 'm_name' FROM al_app_t app LEFT OUTER JOIN al_merchant_t m ON app.mcode = m.mcode WHERE app.app_key = '{$db_appkey}' AND app.mcode = '{$db_mcode}'";
-echo $sql;	
 	$row = @mysql_fetch_assoc(mysql_query($sql, $conn));
 	if ( !in_array( $row['app_exec_type'], array('I', 'E', 'S') ) ) {
 		include "merchant-campaign-modify-web.php";
